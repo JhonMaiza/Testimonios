fcd7bc205cd3728328e7e5be9ec1fd59cd9b539d
JhonMaiza <jhonmaiza@outlook.com>
2023-12-13 21:48:47 -0500

    Agregando etiqueta strong en el testimonio

--- src/componentes/Testimonio.js
+++ src/componentes/Testimonio.js
@@ -6,10 +6,10 @@ export function Testimonio(props){
     <div className='contenedor-testimonio'>
       <img className='imagen-testimonio'
       src={require(`../imagenes/testimonio-${props.imagen}.jpg`)}
-      alt='Foto de Emma'/>
+      alt={`Foto de ${props.nombre}`}/>
       <div className='contenedor-texto-testimonio'>
-        <p className='nombre-testimonio'>{props.nombre} en {props.pais}</p>
-        <p className='cargo-testimonio'>{props.cargo} en {props.empresa}</p>
+        <p className='nombre-testimonio'><strong>{props.nombre}</strong> en {props.pais}</p>
+        <p className='cargo-testimonio'>{props.cargo} en <strong>{props.empresa}</strong> </p>
         <p className='texto-testimonio'>"{props.testimonio}"</p>
       </div>
     </div>
